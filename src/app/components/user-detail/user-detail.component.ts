66e52704d0fc468a3185d7e69e3532752d43523d
william <william.steinkedemello@gmail.com>
2022-12-08 18:19:46 +0100

    logout related issue in user-detail.component fixed

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CrendetialsService } from 'src/app/_services/crendetials.service';
 import { UserInfo } from '../../models/UserInfo.dto';
 import { UserService } from '../../_services/user.service';
 
@@ -12,13 +13,23 @@ export class UserDetailComponent implements OnInit {
   defaultPicture = '../../../assets/no-profile-img.jpg';
   actualLevel: number = 1;
   userFound?: UserInfo | null;
-  constructor(private route: ActivatedRoute) {}
+  isUserLoggedin = false;
+  constructor(
+    private credentialsService: CrendetialsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
   ngOnInit() {
     this.route.params.subscribe((params) => {
       console.log('PARAMS: ', params);
       this.defaultPicture = params['picturePath'];
       this.userFound = params as UserInfo;
       this.actualLevel = params['userLevel'];
+    });
+
+    this.credentialsService.isUserLoggedIn$.subscribe((isLoggedin) => {
+      if (!isLoggedin) this.router.navigate(['/']);
+      this.isUserLoggedin = isLoggedin;
     });
   }
 }
